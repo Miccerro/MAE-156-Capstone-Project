80e2b49b071eb9d9a990aee14b21ddd569719867
emontejofrancisco <emontejofrancisco@ucsd.edu>
2024-04-11 19:39:22 -0700

    XML example
    These two files interact with one another to show how we can parse the XML messages. We need to know the XML messages that will be received to act accordingly

---
+++ XML/GDS_Server.py
@@ -0,0 +1,71 @@
+#TCP Server Side, GDS (via corenrstone)
+import socket
+import xml.etree.ElementTree as ET
+import time
+
+# Define constants
+HOST_IP = socket.gethostbyname(socket.gethostname())
+HOST_PORT = 12345
+ENCODER = "utf-8"
+BYTESIZE = 1024
+
+#Create server socket, bind it to ip/port, and listen
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST_IP, HOST_PORT))
+server_socket.listen()
+
+#Accept any incoming connection and let them know they are connected
+print("Server is running... \n")
+client_socket, client_address = server_socket.accept()
+client_socket.send("You are connected to the server...\n".encode(ENCODER))
+
+#Infinite loop to Send/Recieve messages as server
+while True:
+    #Recieve information from the client
+    message = client_socket.recv(BYTESIZE).decode(ENCODER)
+
+    # #Quit if client socket wants to quit, else display the message
+    # if message == "quit":
+    #     client_socket.send("quit".encode(ENCODER))
+    #     print("\n Ending the chat (Server side)... Goodbye!!")
+    #     break
+    # else:
+    #     print(f"\n{message}")
+    #     message = input("Message: ")
+    #     client_socket.send(message.encode(ENCODER))
+
+    #Examples of possible XML messages that the GDS will send (not 100%) (may be another format)
+    xml_message1 = '<Sequence Name="Prep Sample" Running="false" LastReturnResult="" />'
+    xml_message2 = '<Sequence Name="Load Sample" Running="false" LastReturnResult="" />'
+    xml_message3 = '<Sequence Name="Load Sample Step 1" Running="false" LastReturnResult="Error: pressure evacuation timeout" />'
+    xml_message4 = '<Sequence Name="Load Sample Step 2" Running="false" LastReturnResult="" />'
+    xml_message4 = '<Sequence Name="Load Sample Step 3" Running="false" LastReturnResult="" />'
+    xml_message5 = '<Sequence Name="Ream Anode" Running="false" LastReturnResult="" />'
+    
+    #Lets imagine the GDS takes a while to execute the command
+   #USED FOR REFRENCE: NOTE: <ExecuteSequence Sequence='Ream Anode' />  AT THIS POINBT WE GET THIS XML COMMAND
+    
+    
+    
+    root = ET.fromstring(message)
+    # Read the value of the 'Sequence' attribute
+    sequence_value = root.get('Sequence')
+    if root.tag == "ExecuteSequence":
+        sequence_value = root.get('Sequence')
+        print("Received ExecuteSequence:", sequence_value)
+        response = "<Sequence Name='Ream Anode' Running='true' LastReturnResult='' />"
+        client_socket.send(response.encode())
+
+        # Wait for 8 seconds
+        time.sleep(8)
+        # Send the response
+        response = "<Sequence Name='Ream Anode' Running='complete' LastReturnResult='' />"
+        client_socket.send(response.encode())
+        time.sleep(1000)
+    else:
+            print("Received unknown XML message:", xml_string)
+
+
+
+# Close the socket
+server_socket.close()
---
+++ XML/PC_Client.py
@@ -0,0 +1,82 @@
+#TCP Client Side, PC 
+import socket
+import xml.etree.ElementTree as ET
+import time
+
+
+#Define constants
+DESTINATION_IP = socket.gethostbyname(socket.gethostname())
+DESTINATION_PORT = 12345
+ENCODER = "utf-8"
+BYTESIZE = 1024
+
+#Create client socket and connect to server
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((DESTINATION_IP, DESTINATION_PORT))
+
+#Send/Recieve messages from server
+while True:
+    #Recieve information from the server
+    message = client_socket.recv(BYTESIZE).decode(ENCODER)
+    print(f"\n{message}")
+
+    # #Quit if the connected server wants to quit, else keep sending messages
+    # if message == "quit":
+    #     client_socket.send("quit".encode(ENCODER))
+    #     print("\nEnding the Chat (Client Side)... Goodbye!!")
+    #     break
+    # else:
+    #         print(f"\n{message}")
+    #         message = input("Message: ")
+    #         if message == "Command":
+    #             message = "<ExecuteSequence Sequence='Ream Anode' />"
+    #             client_socket.send(message.encode(ENCODER))
+    #         else:
+    #             client_socket.send(message.encode(ENCODER))
+
+    #Examples of possible XML messages to send to GDS:
+    xml_message1 = "<ExecuteSequence Sequence='Prep Sample' />"
+    xml_message2 = "<ExecuteSequence Sequence='Load Sample' />"
+    xml_message3 = "<ExecuteSequence Sequence='Load Sample Step 1' />"
+    xml_message4 = "<ExecuteSequence Sequence='Load Sample Step 2' />"
+    xml_message5 = "<ExecuteSequence Sequence='Load Sample Step 3' />"
+    xml_message6 = "<ExecuteSequence Sequence='Ream Anode' />"
+
+
+
+    #Assume we send over commands (will figure out later what order they go)
+    #Send over ream 
+    client_socket.send(xml_message6.encode(ENCODER))
+
+    #OUR LISTINE TO ACT AFTER RECIEVING THIS MESSAGE
+    #"<Sequence Name='Ream Anode' Running='true' LastReturnResult='' />"
+    #OR
+    #"<Sequence Name='Ream Anode' Running='true' LastReturnResult='' />"
+
+    xml_message = client_socket.recv(BYTESIZE).decode(ENCODER)
+    print("Received XML message from GDS:", xml_message)
+
+    #parse XML message:
+    root = ET.fromstring(xml_message)
+    running = root.get('Running')
+    print("Running:", running)
+
+    # Wait until 'Running' is 'complete'
+    while running != 'complete':
+        xml_message = client_socket.recv(BYTESIZE).decode(ENCODER)
+        root = ET.fromstring(xml_message)
+        running = root.get('Running')
+        print("Running:", running)
+        print("This proves that we can parse xml messages")
+
+#This is a simple example of pasring XML messages. Will need to start learning the order
+#We can already start psuedocode to layout how we will respond to certain messages. Need aspecific step by step
+
+
+
+
+
+#Close Client Socket
+client_socket.close()
+
+
